ff37c5021b36ca0950347b723b7613e42db244eb
DaniilJustCode <gromdan90@gmail.com>
2024-08-18 13:22:49 +0300

    init commit + class 18.08.24

---
+++ src/test/java/telran/stream/StreamTasksTest.java
@@ -0,0 +1,12 @@
+package telran.stream;
+
+import org.junit.jupiter.api.Test;
+
+public class StreamTasksTest {
+
+    @Test
+    void shuffleTest() {
+        //TODO
+        //testing of method shuffle of StreamTasks class
+    }
+}
