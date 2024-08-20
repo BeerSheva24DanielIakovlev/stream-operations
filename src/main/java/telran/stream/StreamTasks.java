27341df43c058a2d5add9021f9a3e54ae5059a9f
DaniilJustCode <gromdan90@gmail.com>
2024-08-20 14:16:53 +0300

    right homework (not my code)

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -1,11 +1,9 @@
 package telran.stream;
 
 import java.util.Random;
-import java.util.stream.IntStream;
 
 public class StreamTasks {
     public static int[] shuffle(int[] arr) {
-        Random random = new Random();
-        return IntStream.range(0, arr.length).map(i -> arr[random.nextInt(arr.length)]).toArray();
+        return new Random().ints(0, arr.length).distinct().limit(arr.length).map(i -> arr[i]).toArray();
     }
 }
--- src/test/java/telran/stream/StreamTasksTest.java
+++ src/test/java/telran/stream/StreamTasksTest.java
@@ -1,39 +1,32 @@
 package telran.stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
 public class StreamTasksTest {
-
+    private static final int N_RUNS = 100;
+    
     @Test
     void shuffleTest() {
-        int[] original = {1, 2, 3, 4, 5};
-        int[] shuffled = StreamTasks.shuffle(original);
-
-        assertEquals(original.length, shuffled.length);
-
-        Set<Integer> originalSet = new HashSet<>();
-        for (int num : original) {
-            originalSet.add(num);
+        int array[] = {10, 10, 20, 30, 40, 50, 50, 70, 80, 90};
+        int[] arShuffled = StreamTasks.shuffle(array);
+        for (int i = 1; i < N_RUNS; i++ ) {
+            int[] arShuffled2 = StreamTasks.shuffle(array);
+            assertFalse(Arrays.equals(arShuffled, arShuffled2));
+            arShuffled = arShuffled2;
+            runTest(arShuffled, array);
         }
-
-        Set<Integer> shuffledSet = new HashSet<>();
-        for (int num : shuffled) {
-            shuffledSet.add(num);
-        }
-
-        boolean isShuffled = !Arrays.equals(original, shuffled);
-        assertTrue(isShuffled);
-
-        int[] shuffledAgain = StreamTasks.shuffle(original);
-        assertFalse(Arrays.equals(shuffled, shuffledAgain));
+        
+        
+     }
+    
+    private void runTest(int[] shuffled, int[] array) {
+        assertFalse(Arrays.equals(shuffled, array));
+        Arrays.sort(shuffled);
+        assertTrue(Arrays.equals(shuffled, array));
     }
 }
 
